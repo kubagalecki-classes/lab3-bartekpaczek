2431dc9a7dcd9ae56e8e118999a28aab53a7e734
Bartek <bartek.paczek@gmail.com>
2020-11-16 23:16:38 +0100

    Zad 10

--- lab3.cpp
+++ lab3.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
+#include <cmath>
+using namespace std;
+class Figura{
+    protected:
+        double pole=0;
+    public:
+        double get_pole(){
+            return pole;
+        };
+
+        void set_pole(double value){
+            pole = value;
+        };
+        void id(){
+            cout<<"Figura";
+        };
+
+};
+
+class Kolo : public Figura{
+    private:
+        double promien;
+    public:
+        Kolo(double r):promien{r}{
+            pole = 3.14*promien*promien;
+        };
+    void id(){
+            cout<<"Kolo";
+        };
+};
+class Kwadrat : public Figura{
+    private:
+        double bok;
+    public:
+        Kwadrat(double a):bok{a}{
+            pole = bok*bok;
+        };
+    void id(){
+            cout<<"Kwadrat";
+        };
+};
+
+void id(Figura* x){
+   x->id();
+};
 
 int main()
 {
-    puts("Hello, World!");
+    Kwadrat a{5.};
+    id(&a);
 }
