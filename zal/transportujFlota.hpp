bd16763cd8a4c7d6138b0a56541139dc79f0beb1
Bartek <bartek.paczek@gmail.com>
2020-11-18 18:58:50 +0100

    proba1

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,20 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    unsigned int licznikTowaru{0};
+    unsigned int liczniZaglowcow{0};
+    if (towar!=0){
+        Stocznia stocznia{};
+
+        while(towar<licznikTowaru){
+            Statek* s1 = stocznia();
+            licznikTowaru += s1->transportuj();
+            Zaglowiec* test = dynamic_cast<Zaglowiec*>(s1);
+            if(test!=nullptr){
+                liczniZaglowcow++;
+            }       
+            delete s1;
+        };
+    };
+    return liczniZaglowcow;
 }
